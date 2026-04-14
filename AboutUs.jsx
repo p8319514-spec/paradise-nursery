64c595ace2ef985b8d3f76be477a24543a038172
p8319514-spec <p8319514@gmail.com>
2026-04-14 20:24:48 +0530

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>We provide beautiful plants for your home and garden.</p>
+    </div>
+  );
+}
+
+export default AboutUs;

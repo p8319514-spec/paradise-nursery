ffb78b7491300b9d643c2953f3332d91091f3db5
p8319514-spec <p8319514@gmail.com>
2026-04-14 20:36:21 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,9 +2,22 @@ import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Paradise Nursery</h1>
-      <button>Get Started</button>
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>🌿 Paradise Nursery</h1>
+      <p>Your one-stop shop for beautiful indoor plants</p>
+
+      <button
+        style={{
+          padding: "10px 20px",
+          fontSize: "16px",
+          backgroundColor: "green",
+          color: "white",
+          border: "none",
+          borderRadius: "5px"
+        }}
+      >
+        Get Started
+      </button>
     </div>
   );
 }

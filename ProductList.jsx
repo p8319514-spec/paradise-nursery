80b8206d2cf89b0e2c434e69fe0eed7d5051ee7a
p8319514-spec <p8319514@gmail.com>
2026-04-14 21:19:49 +0530

    Refactor ProductList to use Redux for cart management

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,24 +1,43 @@
-import React, { useContext } from "react";
-import { CartContext } from "./CartContext";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { addItem } from "./redux/cartSlice";
 
-const products = [
-  { id: 1, name: "Aloe Vera", price: 10 },
-  { id: 2, name: "Snake Plant", price: 15 },
-  { id: 3, name: "Peace Lily", price: 20 }
+const plantsArray = [
+  {
+    category: "Indoor Plants",
+    plants: [
+      { id: 1, name: "Aloe Vera", price: 10, img: "https://via.placeholder.com/100" },
+      { id: 2, name: "Snake Plant", price: 15, img: "https://via.placeholder.com/100" },
+      { id: 3, name: "Peace Lily", price: 20, img: "https://via.placeholder.com/100" },
+      { id: 4, name: "Spider Plant", price: 12, img: "https://via.placeholder.com/100" },
+      { id: 5, name: "Money Plant", price: 18, img: "https://via.placeholder.com/100" },
+      { id: 6, name: "Fern", price: 14, img: "https://via.placeholder.com/100" }
+    ]
+  }
 ];
 
 function ProductList() {
-  const { addToCart } = useContext(CartContext);
+  const dispatch = useDispatch();
 
   return (
     <div>
       <h2>Products</h2>
 
-      {products.map(p => (
-        <div key={p.id} style={{ border: "1px solid gray", margin: "10px" }}>
-          <h3>{p.name}</h3>
-          <p>${p.price}</p>
-          <button onClick={() => addToCart(p)}>Add to Cart</button>
+      {plantsArray.map(category => (
+        <div key={category.category}>
+          <h3>{category.category}</h3>
+
+          {category.plants.map(plant => (
+            <div key={plant.id} style={{ margin: "10px" }}>
+              <img src={plant.img} alt="" />
+              <h4>{plant.name}</h4>
+              <p>${plant.price}</p>
+
+              <button onClick={() => dispatch(addItem(plant))}>
+                Add to Cart
+              </button>
+            </div>
+          ))}
         </div>
       ))}
     </div>

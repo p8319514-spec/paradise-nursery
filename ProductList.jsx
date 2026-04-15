3c62cb379248f90ff4ed5c5d64b7df25a00322f0
p8319514-spec <p8319514@gmail.com>
2026-04-15 10:03:59 +0530

    Refactor ProductList to use products array

--- ProductList.jsx
+++ ProductList.jsx
@@ -1,19 +1,11 @@
 import React from "react";
 import { useDispatch } from "react-redux";
-import { addItem } from "./redux/cartSlice";
+import { addItem } from "./CartSlice";
 
-const plantsArray = [
-  {
-    category: "Indoor Plants",
-    plants: [
-      { id: 1, name: "Aloe Vera", price: 10, img: "https://via.placeholder.com/100" },
-      { id: 2, name: "Snake Plant", price: 15, img: "https://via.placeholder.com/100" },
-      { id: 3, name: "Peace Lily", price: 20, img: "https://via.placeholder.com/100" },
-      { id: 4, name: "Spider Plant", price: 12, img: "https://via.placeholder.com/100" },
-      { id: 5, name: "Money Plant", price: 18, img: "https://via.placeholder.com/100" },
-      { id: 6, name: "Fern", price: 14, img: "https://via.placeholder.com/100" }
-    ]
-  }
+const products = [
+  { id: 1, name: "Aloe Vera", price: 10 },
+  { id: 2, name: "Snake Plant", price: 15 },
+  { id: 3, name: "Peace Lily", price: 20 },
 ];
 
 function ProductList() {
@@ -21,23 +13,14 @@ function ProductList() {
 
   return (
     <div>
-      <h2>Products</h2>
-
-      {plantsArray.map(category => (
-        <div key={category.category}>
-          <h3>{category.category}</h3>
-
-          {category.plants.map(plant => (
-            <div key={plant.id} style={{ margin: "10px" }}>
-              <img src={plant.img} alt="" />
-              <h4>{plant.name}</h4>
-              <p>${plant.price}</p>
-
-              <button onClick={() => dispatch(addItem(plant))}>
-                Add to Cart
-              </button>
-            </div>
-          ))}
+      <h2>Plant List</h2>
+      {products.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>${item.price}</p>
+          <button onClick={() => dispatch(addItem(item))}>
+            Add to Cart
+          </button>
         </div>
       ))}
     </div>
